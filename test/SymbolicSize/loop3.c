3cc547fd6f55d27a94dbb92e62208766d7f6c791
David Trabish <davidtr1037@gmail.com>
2020-12-06 13:24:59 +0200

    adding test

---
+++ test/SymbolicSize/loop3.c
@@ -0,0 +1,29 @@
+// RUN: %clang %s -emit-llvm %O0opt -c -o %t.bc
+// RUN: rm -rf %t.klee-out
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include <klee/klee.h>
+
+#define MAX_SIZE (1)
+
+void f() {
+    size_t n;
+    klee_make_symbolic(&n, sizeof(n), "n");
+    klee_assume(n <= MAX_SIZE);
+
+    unsigned char *p = malloc(n);
+    for (unsigned i = 0; i < n; i++) {
+        p[i] = 7;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    f();
+    f();
+    return 0;
+}
