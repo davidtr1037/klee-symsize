d3edabc1f3602d237df30052329e712270be75d1
David Trabish <davidtr1037@gmail.com>
2020-12-16 10:22:21 +0200

    updating tests

--- test/SymbolicSize/loop10.c
+++ test/SymbolicSize/loop10.c
@@ -1,8 +1,6 @@
 // RUN: %clang %s -emit-llvm %O0opt -c -o %t.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out -libc=uclibc --use-loop-merge -use-optimized-merge=1 -allocate-sym-size -capacity=200 --search=dfs %t.bc 2>&1 | FileCheck %s
-
-// CHECK: KLEE: merged 3 states (early = 1)
+// RUN: %klee --output-dir=%t.klee-out -libc=uclibc --use-loop-merge -use-optimized-merge=1 -allocate-sym-size -capacity=200 --search=dfs %t.bc 2>&1
 
 #include <stdio.h>
 #include <stdlib.h>
--- test/SymbolicSize/loop5.c
+++ test/SymbolicSize/loop5.c
@@ -1,8 +1,8 @@
 // RUN: %clang %s -emit-llvm %O0opt -c -o %t.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out -libc=uclibc --use-loop-merge -use-optimized-merge=1 -allocate-sym-size -capacity=200 --search=dfs %t.bc 2>&1 | FileCheck %s
+// RUN: %klee --output-dir=%t.klee-out -libc=uclibc --use-loop-merge -use-optimized-merge=1 -allocate-sym-size -capacity=4 --search=dfs %t.bc 2>&1 | FileCheck %s
 
-// CHECK: KLEE: merged 3 states (early = 1)
+// CHECK: KLEE: merged 2 states (early = 2)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,19 +12,14 @@
 
 #include <klee/klee.h>
 
-#define MAX_SIZE (10)
 
 int main(int argc, char *argv[]) {
     size_t n;
     klee_make_symbolic(&n, sizeof(n), "n");
-    klee_assume(n <= MAX_SIZE);
 
     unsigned char *p = malloc(n);
     for (unsigned i = 0; i < n; i++) {
-        p[i] = 7;
-        if (i == 2) {
-            abort();
-        }
+        p[i * 2] = 7;
     }
 
     return 0;
