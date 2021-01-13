aac1b8d2b62968f014db0011d2e1f4ccf490dc93
David Trabish <davidtr1037@gmail.com>
2021-01-13 13:29:30 +0200

    adding test

---
+++ test/SymbolicSize/loop12.c
@@ -0,0 +1,33 @@
+// RUN: %clang %s -emit-llvm %O0opt -c -o %t.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out -libc=uclibc --use-loop-merge -use-optimized-merge=1 -allocate-sym-size -capacity=200 --search=dfs %t.bc 2>&1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include <klee/klee.h>
+
+#define MAX_SIZE (10)
+
+unsigned char buffer[MAX_SIZE] = {0,};
+
+int main(int argc, char *argv[]) {
+    size_t n;
+    klee_make_symbolic(&n, sizeof(n), "n");
+    klee_assume(n <= MAX_SIZE);
+
+    unsigned char *p = malloc(n);
+    for (unsigned i = 0; i < n; i++) {
+        p[i] = 7;
+        if (i == 2) {
+            return 1;
+        }
+    }
+
+    free(p);
+
+    return 0;
+}
