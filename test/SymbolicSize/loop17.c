82db76c8e0df81ce649d5b196db8dd2a943b1431
David Trabish <davidtr1037@gmail.com>
2021-01-28 11:10:20 +0200

    updating tests

--- test/SymbolicSize/loop17.c
+++ test/SymbolicSize/loop17.c
@@ -1,7 +1,8 @@
 // RUN: %clang %s -emit-llvm %O0opt -c -o %t.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out -libc=uclibc -optimize-array-values-by-tracking=1 -validate-merge -use-loop-merge -use-optimized-merge=1 -allocate-sym-size -capacity=200 --search=dfs %t.bc 2>&1
+// RUN: %klee --output-dir=%t.klee-out -libc=uclibc -optimize-array-values-by-tracking=1 -validate-merge -use-loop-merge -loop-limit=no-call -use-optimized-merge=1 -allocate-sym-size -capacity=200 --search=dfs %t.bc 2>&1
 
+/* TODO: test with -loop-limit=none */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
